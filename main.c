39cccdac8b81e0006f3a47ba7f05377a785b0512
Sigida Oleg <gopogiest@gmail.com>
2022-06-10 00:13:20 +0300

    pyat

--- main.c
+++ main.c
@@ -5,10 +5,13 @@ int main()
 	int a;
 	int b;
 	int sum;
+	int diff;
 	printf("\nvvedite a:");
 	scanf("%d",&a);
 	printf("\nvvedite b:");
 	scanf("%d",&b);
 	sum= a+b;
+	diff=a-b;
 	printf("\nSUM(a+b) = %d",sum);
+	printf("\nDifference(a-b)=%d",diff);
 }
